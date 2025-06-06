ec5577cfb91b16405438faa89cce0a2be44ebbdb
Brian Khong <jskhong97@gmail.com>
2025-06-06 14:44:38 +0800

    Add submit button

--- src/TodoData.jsx
+++ src/TodoData.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 
 async function fetchTodoData(id) {
     const response = await fetch(
@@ -12,30 +12,35 @@ function Todo({ todoData }) {
     return (
         <div>
             <h2>Title: {todoData.title}</h2>
-            <h2>Completed: {todoData.completed ? "Completed" : "Not Completed"}</h2>
+            <h2>Completed: {todoData.completed ? 'Completed' : 'Not Completed'}</h2>
         </div>
-    )
+    );
 }
 
+//Run after submit button is click
 export default function TodoData() {
-    const [userInput, setUserInput] = useState("")
+    const [userInput, setUserInput] = useState('');
     const [todoData, setTodoData] = useState([]);
     const [isLoading, setIsLoading] = useState(false);
 
+    function Submit() {
+        setUserInput(input.value);
+    }
+
     useEffect(() => {
         if (userInput) {
-            setIsLoading(true)
+            setIsLoading(true);
             fetchTodoData(userInput)
-                .then(data => setTodoData(data))
-                .catch(error => console.log(error))
-                .finally(() => setIsLoading(false))
+                .then((data) => setTodoData(data))
+                .catch((error) => console.log(error))
+                .finally(() => setIsLoading(false));
         }
-    }, [userInput]
-    )
+    }, [userInput]);
 
     return (
         <div>
-            <input type="text" value={userInput} onChange={(event) => setUserInput(event.target.value)} placeholder='Please enter an ID' />
+            <input id="input" type="text" placeholder="Please enter an ID" />
+            <button onClick={Submit}>Submit</button>
             {isLoading && <p>Loading...</p>}
             {todoData && todoData.title && (
                 <>
@@ -43,5 +48,35 @@ export default function TodoData() {
                 </>
             )}
         </div>
-    )
+    );
 }
+
+// Run when input is change
+// export default function TodoData() {
+//   const [userInput, setUserInput] = useState("")
+//   const [todoData, setTodoData] = useState([]);
+//   const [isLoading, setIsLoading] = useState(false);
+
+//   useEffect(() => {
+//       if (userInput) {
+//           setIsLoading(true)
+//           fetchTodoData(userInput)
+//               .then(data => setTodoData(data))
+//               .catch(error => console.log(error))
+//               .finally(() => setIsLoading(false))
+//       }
+//   }, [userInput]
+//   )
+
+//   return (
+//       <div>
+//           <input type="text" value={userInput} onChange={(event) => setUserInput(event.target.value)} placeholder='Please enter an ID' />
+//           {isLoading && <p>Loading...</p>}
+//           {todoData && todoData.title && (
+//               <>
+//                   <Todo todoData={todoData} />
+//               </>
+//           )}
+//       </div>
+//   )
+// }
